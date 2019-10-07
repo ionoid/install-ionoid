c53bc9fed4b43bb987bcb4458998e9fd30fee4ee
Djalal Harouni <tixxdz@opendevices.io>
2019-10-07 16:50:21 +0100

    deploy: improve deploy script
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

--- deploy_ionoid_tools.bash
+++ deploy_ionoid_tools.bash
@@ -13,7 +13,7 @@ ssh -p $SSHPORT $USER@$SERVER rm -fr $TARGET
 ssh -p $SSHPORT $USER@$SERVER mkdir -p $TARGET
 
 # Prepare filesystem
-ssh -p $SSHPORT $USER@$SERVER sudo mkdir -p /mnt/storage/$SERVER/www/tools/${TARGET}/
+ssh -p $SSHPORT $USER@$SERVER sudo mkdir -p /var/www/${SERVER}/tools/${TARGET}/
 
 scp -pr -P $SSHPORT install-ionoid-sealos-manager-sdk.bash $USER@$SERVER:~/${TARGET}
 scp -pr -P $SSHPORT install-tools.bash $USER@$SERVER:~/${TARGET}
@@ -22,9 +22,9 @@ scp -pr -P $SSHPORT ionoid-parse-machine.bash $USER@$SERVER:~/${TARGET}
 scp -pr -P $SSHPORT LICENSE $USER@$SERVER:~/${TARGET}
 scp -pr -P $SSHPORT README.md $USER@$SERVER:~/${TARGET}
 
-ssh -p $SSHPORT $USER@$SERVER sudo -E cp -f -r $HOME/${TARGET}/ /mnt/storage/${SERVER}/www/tools/
+ssh -p $SSHPORT $USER@$SERVER sudo -E cp -f -r $HOME/${TARGET} /var/www/${SERVER}/tools/
 
 # Lets fix permissions in cases
-ssh -p $SSHPORT $USER@$SERVER sudo chown -R www-data.www-data /mnt/storage/$SERVER/www/tools/${TARGET}/
+ssh -p $SSHPORT $USER@$SERVER sudo chown -R www-data.www-data /var/www//${SERVER}/tools/${TARGET}/
 
 exit 0
