6745aef731c0e48e989c633133bbb21c3def9fbb
Djalal Harouni <tixxdz@opendevices.io>
2019-10-07 11:44:53 +0100

    ionoid: add parse machine helper
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

---
+++ ionoid-parse-machine.bash
@@ -0,0 +1,31 @@
+function validate_arch() {
+        machine=$1
+
+        # lets reset to empty
+        MACHINE=""
+        if [ "$machine" != "arm6" ] && \
+           [ "$machine" != "arm7" ] && \
+           [ "$machine" != "arm64" ] && \
+           [ "$machine" != "amd64" ] && \
+           [ "$machine" != "x86" ]; then
+                echo "$COMMAND: ARCH '$machine' value not supported." >&2
+                return
+        fi
+
+        MACHINE=$machine
+}
+
+function parse_machine() {
+
+        if [ -n ${MACHINE} ]; then
+                validate_arch ${MACHINE}
+                return
+        fi
+
+        if [ ! -z ${CONFIG} ] && [ -f ${CONFIG} ]; then
+                arch=$(jq -r .API_PROJECT_DEVICE_ARCH ${CONFIG})
+                if [ "$arch" != "null" ]; then
+                        validate_arch $arch
+                fi
+        fi
+}
